1d955447f8da2a24d0ffdeb4b5fc1379779efde7
Deekshitha <deekshithagowda926@gmail.com>
2024-12-22 23:06:42 +0530

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by ATEU on 22/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
